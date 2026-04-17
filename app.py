2d5e60dcccca7daa8f73d0d0b7bf68592a555e73
SHALINI SAURAV <shalinisourav07@gmail.com>
2026-04-17 23:05:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PIL import Image
+from model.blip import get_caption
+from model.llm import ask_llama
+from utils.prompt import build_prompt
+
+st.set_page_config(page_title="Multimodal AI Agent")
+
+st.title("🧠 Multimodal AI Agent")
+st.write("Upload an image and ask a question")
+
+text = st.text_input("Enter your question:")
+image = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
+
+if st.button("Analyze"):
+    if text and image:
+        img = Image.open(image)
+
+        with st.spinner("Processing..."):
+            caption = get_caption(img)
+            prompt = build_prompt(text, caption)
+            response = ask_llama(prompt)
+
+        st.subheader("📸 Image Caption:")
+        st.write(caption)
+
+        st.subheader("💬 AI Response:")
+        st.write(response)
+    else:
+        st.warning("Please provide both text and image!")
